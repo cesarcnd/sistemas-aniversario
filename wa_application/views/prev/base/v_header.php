eae5c2858dd7d9962306d4118e81c025042b5bc0
Cesar Nicho <cesar1992.12.16@gmail.com>
2014-07-27 12:23:28 -0500

    header 100%

--- wa_application/views/prev/base/v_header.php
+++ wa_application/views/prev/base/v_header.php
@@ -1,6 +1,13 @@
 <div class="b-body">
-	<div>
-		<img src="<?php echo base_url()?>resource/image/utilities/organization/unjfsc.png" alt="Universidad Nacional José Faustino Sanchez Carrion">
+	<div class="display-inline unjfsc">
+		<a href="http://web.unjfsc.edu.pe/site/" target="_blank">
+			<img src="<?php echo base_url()?>resource/image/utilities/organization/unjfsc.png" alt="Universidad Nacional José Faustino Sanchez Carrion">
+		</a>
+	</div><!--
+	--><div class="display-inline social">
+		<div class="display-inline text">S&iacute;guenos en : </div>
+		<a href="https://www.facebook.com/AniversarioIngenieriaDeSistemasUnjfsc?fref=ts" target="_blank" class="social_network display-inline facebook"></a>
+		<a href="https://twitter.com/" target="_blank" class="social_network display-inline twitter"></a>
+		<a href="https://www.youtube.com/" target="_blank" class="social_network display-inline youtube"></a>
 	</div>
-	<div></div>
 </div>
