ddac09a69f4bc6a86fc8fa621da059e154ff25c5
Cesar Nicho <cesar1992.12.16@gmail.com>
2014-07-26 19:08:58 -0500

    maquetado

--- wa_application/views/prev/v_portal.php
+++ wa_application/views/prev/v_portal.php
@@ -1,1 +1,24 @@
-Aqui estara la web.
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>XXI Aniversario de Ingenieria de Sistemas</title>
+		<link rel="stylesheet" href="<?php echo base_url()?>resource/css/base.css" type="text/css">
+		<link rel="stylesheet" href="<?php echo base_url()?>resource/css/style.css" type="text/css">
+	</head>
+	<body>
+		<section class="b-all-body">
+			<header id="header"></header>
+			<nav id="browser"></nav>
+			<section class="b-body">
+				<section id="front"></section>
+				<section id="public"></section>
+				<section id="first_block"></section>
+				<section id="second_block"></section>
+				<section id="third_block"></section>	
+			</section>
+			<section id="institute"></section>
+			<footer id="footer"></footer>
+		</section>
+	</body>
+</html>
