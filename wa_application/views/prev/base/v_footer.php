ed12dfcd76794bd544eae0471ad109bf3c0c1580
luisJC <luischavezmaita9313@gmail.com>
2014-08-02 18:25:25 -0500

    footer colores

---
+++ wa_application/views/prev/base/v_footer.php
@@ -0,0 +1,38 @@
+<div class="footer_pie">
+	<div class="colores">
+	<div  class="display-inline color1"></div><!--
+	--><div class="display-inline color2"></div><!--
+	--><div class="display-inline color3"></div><!--
+	--><div class="display-inline color4"></div><!--
+	--><div class="display-inline color5"></div><!--
+	--><div class="display-inline color6"></div><!--
+	--><div class="display-inline color7"></div><!--
+	
+	--><div class="display-inline color8"></div><!--
+	
+	--><div class="display-inline color9"></div><!--
+	
+	--><div class="display-inline color10"></div><!--
+	
+	
+	--></div>
+	<div id="footer_left">
+		<div class="titulo">La Comision</div></br>
+		<div class="contenido">
+		Esta página fué realizada por los alumnos del IX ciclo de la 
+		E.A.P Ingenieria de Sistemas de la Facultad de
+		Ingeniería Industrial, Sistemas e Informatica
+		</div>
+		<div class="footer_left_logos">
+			<a href="https://www.facebook.com/AniversarioIngenieriaDeSistemasUnjfsc?fref=ts" target="_blank" class="sociales display-inline footer_facebook"></a>
+			<a href="https://twitter.com/" target="_blank" class="sociales display-inline footer_googleplus"></a>
+			<a href="https://www.youtube.com/" target="_blank" class="sociales display-inline footer_twitter"></a>
+		</div>
+	</div>
+	<div id="footer_center">
+
+		<div id="footer_center_a"></div>
+		<div id="footer_center_b"></div>
+	</div>
+	<div id="footer_right"></div>
+</div>
