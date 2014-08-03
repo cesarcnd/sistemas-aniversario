c84d155d4c974a2d16258b1f1e32edccb854af75
SusanMR <susan93c@gmail.com>
2014-08-02 21:13:57 -0500

    Cargar evento

--- wa_application/controllers/prev/c_portal.php
+++ wa_application/controllers/prev/c_portal.php
@@ -6,4 +6,16 @@ class C_portal extends CI_Controller {
 	{
 		$this->load->view('prev/v_portal');
 	}
+
+	function cargar_eventacademy()
+	{	$query = "CALL SPRCNSEvent_Academy()";
+		$query=$this->db->query($query);
+		return $query->result_array();
+
+	}
+
+	function cargar_event()
+	{	$this->DATA['eventos']=$this->cargar_eventacademy();
+		echo $this->load->views('prev/module/list/inscription/l_inscription',$this->DATA);
+	}
 }
