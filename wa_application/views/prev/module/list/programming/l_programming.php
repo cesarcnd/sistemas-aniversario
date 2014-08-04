aefef31442d0bd4054b4deace9983962b7c1f4d8
Diego Vicente <xxdiego669xx@gmail.com>
2014-08-04 01:00:18 -0500

    programacion

--- wa_application/views/prev/module/list/programming/l_programming.php
+++ wa_application/views/prev/module/list/programming/l_programming.php
@@ -1,6 +1,6 @@
-<nav>
+<nav class="display-inline top">
 	<?php $this->load->view('prev/module/list/programming/l_nav_programming') ?>
 </nav>
-<section>
+<section class="display-inline top">
 	<?php $this->load->view('prev/module/list/programming/l_body_programming') ?>
 </section>
