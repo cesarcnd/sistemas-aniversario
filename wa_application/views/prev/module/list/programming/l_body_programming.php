a4d24da59392fe133986513b8fb8a12b1a4b3d8a
Diego Vicente <xxdiego669xx@gmail.com>
2014-08-04 02:02:13 -0500

    Programacion y Area Informativa - 45%

--- wa_application/views/prev/module/list/programming/l_body_programming.php
+++ wa_application/views/prev/module/list/programming/l_body_programming.php
@@ -1,3 +1,24 @@
-<div>Item 1</div>
-<div>Item 2</div>
-<div>Item 3</div>
+<div class="item-programming">
+	<div class="display-inline top image">img</div><!-- 
+ --><div class="display-inline top content">
+	 	<p class="title">Campeonato Relampago Sistema Deportes</p>
+	 	<p><span class="sistemas_ico_clock"></span> 15/08/14 | 9:30 am</p>
+	 	<p><span class="sistemas_ico_location"></span> Explanada Universidad</p>
+ 	</div>
+</div>
+<div class="item-programming">
+	<div class="display-inline top image">img</div><!-- 
+ --><div class="display-inline top content">
+	 	<p class="title">Campeonato Relampago Sistema Deportes</p>
+	 	<p><span class="sistemas_ico_clock"></span> 15/08/14 | 9:30 am</p>
+	 	<p><span class="sistemas_ico_location"></span> Explanada Universidad</p>
+ 	</div>
+</div>
+<div class="item-programming">
+	<div class="display-inline top image">img</div><!-- 
+ --><div class="display-inline top content">
+	 	<p class="title">Campeonato Relampago Sistema Deportes</p>
+	 	<p><span class="sistemas_ico_clock"></span> 15/08/14 | 9:30 am</p>
+	 	<p><span class="sistemas_ico_location"></span> Explanada Universidad</p>
+ 	</div>
+</div>
--- wa_application/views/prev/module/list/programming/l_programming.php
+++ wa_application/views/prev/module/list/programming/l_programming.php
@@ -1,6 +1,6 @@
 <nav class="display-inline top">
 	<?php $this->load->view('prev/module/list/programming/l_nav_programming') ?>
-</nav>
-<section class="display-inline top">
+</nav><!-- 
+ --><section class="display-inline top body-programming">
 	<?php $this->load->view('prev/module/list/programming/l_body_programming') ?>
 </section>
