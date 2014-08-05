60884f586ae9f2831738785a15f69e76bc05f239
NatyCM <natiihhH_piscis@hotmail.com>
2014-08-05 02:49:25 -0500

    Revert "instituciones"
    This reverts commit a36df322f69f3d97d72484f650937f40b4c9efa6.

--- wa_application/views/prev/module/v_institute.php
+++ wa_application/views/prev/module/v_institute.php
@@ -1,35 +1,8 @@
-<?php $institutes = array(
-				array('img'=>'interbank.png','url'=>'','name_institute'=>""),
-				array('img'=>'microsoft.png','url'=>'','name_institute'=>""),
-				array('img'=>'mozila-peru.png','url'=>'','name_institute'=>""),
-				array('img'=>'smperu.png','url'=>'','name_institute'=>""),
-				array('img'=>'gobierno-regional.png','url'=>'','name_institute'=>"")) ?>
-<script src="<?php echo base_url()?>resource/js/slides.min.jquery.js"></script>
-<script>
-	$(function(){
-	$('#slide_institute').slides({
-		//preload: true,
-		generateNextPrev: false,
-		pagination:false,
-		play: 2500
-	});
-});
-</script>
-<div class="imginstitute b-body" id="slide_institute">
-	<div class="slides_container" >
-		<div class="slides">
-			<?php foreach ($institutes as $institute) {?>
-				<div class="item display-inline">
-					<div class="institutelogo1 "><img src="<?php echo base_url()?>resource/image/body/institute/<?php echo $institute['img']?>"></div>		
-				</div>
-			<?php } ?>
-		</div>
-		<div class="slides">
-			<?php foreach ($institutes as $institute) {?>
-				<div class="item display-inline">
-					<div class="institutelogo1 "><img src="<?php echo base_url()?>resource/image/body/institute/<?php echo $institute['img']?>"></div>		
-				</div>
-			<?php } ?>
-		</div>
+<div class="imginstitute b-body">
+		<div id="institutelogo1"><img src="<?php echo base_url()?>resource/image/utilities/institucion/interbank.jpg"></div>
+		<div id="institutelogo2"><img src="<?php echo base_url()?>resource/image/utilities/institucion/microsoft.jpg"></div>
+		<div id="institutelogo3"><img src="<?php echo base_url()?>resource/image/utilities/institucion/bancona.jpg"></div>
+		<div id="institutelogo4"><img src="<?php echo base_url()?>resource/image/utilities/institucion/soft.jpg"></div>
+		<div id="institutelogo5"><img src="<?php echo base_url()?>resource/image/utilities/institucion/gr.jpg"></div>
 	</div>
 </div>
