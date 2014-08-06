36131d87868a89182882b4211e3470047c2fe51b
SusanMR <susan93c@gmail.com>
2014-08-05 22:34:57 -0500

    Acceso Directo Avance

---
+++ wa_application/views/prev/module/v_access_direct.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="<?php echo base_url()?>resource/css/base.css" type="text/css">
+</head>
+<body>
+	<div class="direct_access">
+		<div id="conferencias">
+			<div id="link_conferencias">
+				<h2>CONFERENCIAS</h2>
+				<div class="sprite ico_conferencias display-inline"></div>
+				<div class="conferencias_triangle"></div>
+			</div>
+			<div id="content_conferencias">
+				<div class="img_conferencias"></div>
+				<div id="ver_conferencias">
+					<p>Conozca las conferencias</p>
+					
+				</div>
+			</div>
+		</div>
+		<div id="talleres">
+			<div id="link_talleres">
+				<h2>TALLERES</h2>
+				<div class="sprite ico_talleres display-inline"></div>
+			</div>
+			<div id="content_talleres">
+				
+			</div>
+		</div>
+		<div id="concursos">
+			<div id="link_concursos">
+				<h2>CONCURSOS</h2>
+				<div class="sprite ico_concursos display-inline"></div>
+			</div>
+			<div id="content_concursos">
+				
+			</div>
+		</div>
+		<div id="proyectos">
+			<div id="link_proyectos">
+				<h2>PROYECTOS</h2>
+				<div class="sprite ico_proyectos display-inline"></div>
+			</div>
+			<div id="content_proyectos">
+				
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html>
