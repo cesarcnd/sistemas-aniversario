8f3daee85e29c8708e0d694a9cc0efe6cb43ac90
luisJC <luischavezmaita9313@gmail.com>
2014-08-06 12:39:05 -0500

    avanze footer

--- wa_application/views/prev/base/v_footer.php
+++ wa_application/views/prev/base/v_footer.php
@@ -16,23 +16,53 @@
 	
 	
 	--></div>
-	<div id="footer_left">
+	<div class="contenerpie">
+	<div class="display-inline footer_left">
 		<div class="titulo">La Comision</div></br>
 		<div class="contenido">
 		Esta página fué realizada por los alumnos del IX ciclo de la 
 		E.A.P Ingenieria de Sistemas de la Facultad de
 		Ingeniería Industrial, Sistemas e Informatica
 		</div>
-		<div class="footer_left_logos">
+		<div class="display-inline footer_left_logos">
 			<a href="https://www.facebook.com/AniversarioIngenieriaDeSistemasUnjfsc?fref=ts" target="_blank" class="sociales display-inline footer_facebook"></a>
 			<a href="https://twitter.com/" target="_blank" class="sociales display-inline footer_googleplus"></a>
 			<a href="https://www.youtube.com/" target="_blank" class="sociales display-inline footer_twitter"></a>
 		</div>
 	</div>
-	<div id="footer_center">
+	<div class="display-inline footer_center">
 
-		<div id="footer_center_a"></div>
-		<div id="footer_center_b"></div>
+		<div class="display-inline footer_center_a">Ing. Sistemas</div></br>
+		<div class="display-inline footer_center_b1">
+			<div id="parrafo1">
+			<p>Proyecto</p></br>
+			<p>Feria de Proyectos </p></br>
+			<p>Programacion</p></br>
+			<p>Sistemas...</p>
+		</div>
+		</div>
+		<div class="display-inline footer_center_b2">
+			<div id="parrafo2">
+			<p>Contacto</p></br>
+			<p>(01)237-16555</p></br>
+			<p>Proyecto 3</p></br>
+			</div>
+		</div>
+	
 	</div>
-	<div id="footer_right"></div>
+	<div class="display-inline footer_right" style="background-color:white">
+
+	<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FAniversarioIngenieriaDeSistemasUnjfsc%3Ffref%3Dts&amp;width&amp;height=225&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:290px;" allowTransparency="true">
+
+	</iframe>
+	</div>
+
+</div>
+
+<div class="pie_pagina">
+		
+		<p>Alumnos del IX cilo de Ingenieria de Sistemas de laUNJFSC</p> 
+		<p>2014 todos los derechos
+		</p>
+	</div>
 </div>
