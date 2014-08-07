1c67503512ded51e066afff07056d06d36915ecb
SusanMR <susan93c@gmail.com>
2014-08-06 22:00:45 -0500

    Avance Direct_Access

---
+++ resource/js/collapsible.js
@@ -0,0 +1,9 @@
+$('.link_conferencias').collapsible('accordion', {
+  
+});
+$('.link_concursos').collapsible('accordion-open', {
+  contentOpen: 1
+});
+$('.link_talleres').collapsible('accordion');
+
+$('.content_proyectos').collapsible();
