d28131b34bf7606f9ac1c20d7302b4a99fa290b3
Cesar Nicho <cesar1992.12.16@gmail.com>
2014-08-15 00:49:21 -0500

    feria de proyectos

--- wa_application/views/prev/interior/project/i_project.php
+++ wa_application/views/prev/interior/project/i_project.php
@@ -1,5 +1,23 @@
 <div id="projects">
 	<div class="item-project">
-		<?php $this->load->view('prev/interior/construction'); ?>
+		<div class="ciclo display-inline">IX</div>
+		<div class="content_project display-inline">
+			<div class="title_project"><b>Desarrollo de nuevas TI'cs</b></div>
+			<div class="description">Esto es una applicacion que sera usado para porder Esto es una applicacion que sera usado para porder Esto es una applicacion que sera usado para porder...</div>
+		</div>
+	</div>
+	<div class="item-project">
+		<div class="ciclo display-inline">IX</div>
+		<div class="content_project display-inline">
+			<div class="title_project"><b>Desarrollo de nuevas TI'cs</b></div>
+			<div class="description">Esto es una applicacion que sera usado para porder Esto es una applicacion que sera usado para porder Esto es una applicacion que sera usado para porder...</div>
+		</div>
+	</div>
+	<div class="item-project">
+		<div class="ciclo display-inline">IX</div>
+		<div class="content_project display-inline">
+			<div class="title_project"><b>Desarrollo de nuevas TI'cs</b></div>
+			<div class="description">Esto es una applicacion que sera usado para porder Esto es una applicacion que sera usado para porder Esto es una applicacion que sera usado para porder...</div>
+		</div>
 	</div>
 </div>
